1a59a9da2cafa92f36a211cc33de1f28be0f308a
birdx0810 <birdx0810@gmail.com>
2020-02-22 22:18:10 +0800

    Template for header and footer (in progress)

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use('/css', express.static('css'));
+app.use('/lib/fonts', express.static('lib/fonts'));
+
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
