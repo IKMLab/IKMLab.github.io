4686db14497ff7d49aa8aa331c228f0dd41a4201
stonelin <jason840708@gmail.com>
2020-09-18 15:33:52 +0800

    add 109 new master member

--- src/res/data/member.js
+++ src/res/data/member.js
@@ -62,6 +62,16 @@ import ZhenYiWu
   from 'src/res/image/member/Zhen-Yi-Wu.png'
 import ZiKuanHuang
   from 'src/res/image/member/Zi-Kuan-Huang.png'
+import HanChiang
+  from 'src/res/image/member/Han-Chiang.png'
+import ChienLiangLiu
+  from 'src/res/image/member/Chien-Liang-Liu.png'
+import MingJiChen
+  from 'src/res/image/member/Ming-Ji-Chen.png'
+import SzuTungLin
+  from 'src/res/image/member/Szu-Tung-Lin.png'
+import HsiuWenLi
+  from 'src/res/image/member/Hsiu-Wen-Li.png'
 
 export const memberData = [
   // PhD Students
@@ -211,6 +221,41 @@ export const memberData = [
     dept: 2,
     deg: 1,
     image: KuanYuChen,
+  },
+  {
+    zh: '江昇翰',
+    en: 'Han Chiang',
+    dept: 1,
+    deg: 1,
+    image: HanChiang,
+  },
+  {
+    zh: '劉建良',
+    en: 'Chien-Liang Liu',
+    dept: 0,
+    deg: 1,
+    image: ChienLiangLiu,
+  },
+  {
+    zh: '黎修文',
+    en: 'Hsiu-Wen Li',
+    dept: 0,
+    deg: 1,
+    image: HsiuWenLi,
+  },
+  {
+    zh: '陳明吉',
+    en: 'Ming-Ji Chen',
+    dept: 0,
+    deg: 1,
+    image: MingJiChen,
+  },
+  {
+    zh: '林思彤',
+    en: 'Szu-Tung Lin',
+    dept: 0,
+    deg: 1,
+    image: SzuTungLin,
   },
   // Graduate PhDs
   {
