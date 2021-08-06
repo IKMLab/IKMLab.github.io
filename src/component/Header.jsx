d37de702ff9c5af5a743b1e7f81b5eb96c704f58
wulong <wulongcha93@gmail.com>
2021-08-06 08:35:42 +0800

    modified header.jsx ListItem

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,18 +1,11 @@
 import AppBar from '@material-ui/core/AppBar'
-import Divider from '@material-ui/core/Divider'
 import HomeIcon from '@material-ui/icons/Home'
 import IconButton from '@material-ui/core/IconButton'
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
 import Link from '@material-ui/core/Link'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import MenuIcon from '@material-ui/icons/Menu'
 import PeopleIcon from '@material-ui/icons/People'
 import PersonIcon from '@material-ui/icons/Person'
 import React from 'react'
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import Toolbar from '@material-ui/core/Toolbar'
 
 import HeaderStyle from 'src/style/Header.module.scss'
@@ -40,14 +33,45 @@ export default function Header() {
         className={HeaderStyle['toolbar']}
         variant='regular'
       >
-        <IconButton
-          aria-label='button'
-          className={HeaderStyle['menu-icon']}
-          color='inherit'
-          edge='start'
-          onClick={toggleDrawer(true)}
-        >
-          <MenuIcon />
+        <IconButton>
+           <HomeIcon
+                  className={HeaderStyle['hotbar-icon']}
+                  titleAccess='Home' />
+           <Link
+                  className={HeaderStyle['hotbar-text']}
+                  href={`${PUBLIC_URL}home.html`}>
+                  &#160;Home
+           </Link>
+        </IconButton>
+        <IconButton>
+            <PersonIcon
+                  className={HeaderStyle['hotbar-icon']}
+                  titleAccess='Advisor' />
+            <Link
+                  className={HeaderStyle['hotbar-text']}
+                  href={`${PUBLIC_URL}advisor.html`}>
+                  &#160;Advisor
+            </Link>
+        </IconButton>
+        <IconButton>
+            <PeopleIcon
+                  className={HeaderStyle['hotbar-icon']}
+                  titleAccess='Member' />
+            <Link
+                  className={HeaderStyle['hotbar-text']}
+                  href={`${PUBLIC_URL}member.html`}>
+                  &#160;Member
+            </Link>
+        </IconButton>
+        <IconButton>
+            <LibraryBooksIcon
+                  className={HeaderStyle['hotbar-icon']}
+                  titleAccess='Research' />
+            <Link
+                  className={HeaderStyle['hotbar-text']}
+                  href={`${PUBLIC_URL}research.html`}>
+                  &#160;Research
+            </Link>
         </IconButton>
         <Link
           className={HeaderStyle['logo-link']}
@@ -58,78 +82,6 @@ export default function Header() {
             role='img'
             src={logoImage} />
         </Link>
-        <SwipeableDrawer
-          anchor='left'
-          onClose={toggleDrawer(false)}
-          onOpen={toggleDrawer(true)}
-          open={isOpen}
-          PaperProps={{
-            className: HeaderStyle['drawer-paper'],
-          }}
-          variant='temporary'
-        >
-          <List className={HeaderStyle['list']}>
-            <ListItem>
-              <ListItemIcon>
-                <HomeIcon
-                  className={HeaderStyle['list-item-icon']}
-                  titleAccess='Home' />
-              </ListItemIcon>
-              <ListItemText>
-                <Link
-                  className={HeaderStyle['list-item-text']}
-                  href={`${PUBLIC_URL}home.html`}>
-                  Home
-                </Link>
-              </ListItemText>
-            </ListItem>
-            <Divider className={HeaderStyle['list-item-divider']} />
-            <ListItem>
-              <ListItemIcon>
-                <PersonIcon
-                  className={HeaderStyle['list-item-icon']}
-                  titleAccess='Advisor' />
-              </ListItemIcon>
-              <ListItemText>
-                <Link
-                  className={HeaderStyle['list-item-text']}
-                  href={`${PUBLIC_URL}advisor.html`}>
-                  Advisor
-                </Link>
-              </ListItemText>
-            </ListItem>
-            <Divider className={HeaderStyle['list-item-divider']} />
-            <ListItem>
-              <ListItemIcon>
-                <PeopleIcon
-                  className={HeaderStyle['list-item-icon']}
-                  titleAccess='Member' />
-              </ListItemIcon>
-              <ListItemText>
-                <Link
-                  className={HeaderStyle['list-item-text']}
-                  href={`${PUBLIC_URL}member.html`}>
-                  Member
-                </Link>
-              </ListItemText>
-            </ListItem>
-            <Divider className={HeaderStyle['list-item-divider']} />
-            <ListItem>
-              <ListItemIcon>
-                <LibraryBooksIcon
-                  className={HeaderStyle['list-item-icon']}
-                  titleAccess='Research' />
-              </ListItemIcon>
-              <ListItemText>
-                <Link
-                  className={HeaderStyle['list-item-text']}
-                  href={`${PUBLIC_URL}research.html`}>
-                  Research
-                </Link>
-              </ListItemText>
-            </ListItem>
-          </List>
-        </SwipeableDrawer>
       </Toolbar>
     </AppBar>
   )
